73051711f6be37b5d854fb26e01baafce9dbc49b
Jacqueline Young <jackiey1966@gmail.com>
2017-03-14 13:38:51 -0400

    initial submit

---
+++ javascript/train.js
@@ -0,0 +1,12 @@
+$(document).ready(function()
+{
+				var config = {
+    			apiKey: "AIzaSyDsIyfAghfH8B4rYtv-BMb0WwB_JrklxC4",
+    			authDomain: "trainscheduler-3f474.firebaseapp.com",
+    			databaseURL: "https://trainscheduler-3f474.firebaseio.com",
+    			storageBucket: "trainscheduler-3f474.appspot.com",
+    			messagingSenderId: "648123629560"
+  				};
+  				firebase.initializeApp(config);
+
+}
